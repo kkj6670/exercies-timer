b6984b983fe26ef22c0cb10c2394cd224db2e29e
kkj6670 <48434120+kkj6670@users.noreply.github.com>
2022-01-13 12:08:37 +0900

    chore: eslint 설정 및 자동화

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+// const { width, height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(47, 52, 55)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hellosee</Text>
+    </View>
+  );
+}
